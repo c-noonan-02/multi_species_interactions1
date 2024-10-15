8380f8d8d82121221f585577ff23e9ee7f072cea
c-noonan-02 <charlottenoonan@outlook.com>
2024-10-15 17:09:37 +0100

    Completed step 4 - three species competition

--- multi_species_interactions_code.R
+++ multi_species_interactions_code.R
@@ -396,3 +396,116 @@ ggplot(data = out_df)+
   theme_bw()
 
 
+# STEP FOUR: THREE SPECIES COMPETITION LOTKA-VOLTERRA MODEL - LIMITING SIMILARITY
+
+# change to a competition model
+parameters <- c(a12=1, a21=0.9, r=0.3, K = 100)
+state <- c(X1=50, X2=10)
+
+# dx1/dt = r1x1 (1 - x1/K1 - a12x2/K1)
+# dx2/dt = r2x2 (1 - x2/K2 - a21x1/K2)
+lotka_volterra_competition <- function(times,state,parameters){ #logistic growth function, that takes a set of parameter values, initial conditions and a time sequence
+  with(as.list(c(state, parameters)), { #"with" is a function that allows us to use the variable names directly - it looks for r, K and P in state and parameters
+    
+    dX1 <- r*X1*(1-(X1+a12*X2)/K) # this is our equation for the prey dynamics, now considering competition
+    dX2 <- r*X2*(1-(X2+a21*X1)/K) # this is the equation for predator dynamics, now considering competition
+    
+    # return the rate of change
+    list(c(dX1, dX2))
+  }) # end with(as.list ...
+}
+
+times <- seq(0,1000,by=0.01)
+
+out <- ode(y=state, times = times, func = lotka_volterra_competition, parms = parameters)
+out_df <- data.frame(out)
+
+ggplot(data = out_df)+
+  geom_line(mapping=aes(x=time,y=X1),color="lightblue") +
+  geom_line(mapping=aes(x=time,y=X2),color="blue") +
+  geom_hline(yintercept=0,color="darkgrey") +
+  geom_vline(xintercept=0,color="darkgrey") +
+  labs(x = "Time", y = "P") +
+  theme_bw()
+
+ggplot(data = out_df)+
+  geom_path(mapping=aes(x=X1,y=X2),color="blue") +
+  geom_hline(yintercept=0,color="darkgrey") +
+  geom_vline(xintercept=0,color="darkgrey") +
+  labs(x = "Species 1", y = "Species 2") +
+  theme_bw()
+
+
+# now we can consider three species
+# the aij parameters will be determined by the relative niches of the species
+# see lecture for alpha equations
+rm(list=ls())
+
+# dx1/dt = r1x1 (1 - (x1 + a12 x2 + a13 x3)/K1)
+# dx2/dt = r2x2 (1 - (x2 + a21 x1 + a32 x3)/K2)
+# dx3/dt = r3x3 (1 - (x3 + a31 x1 + a32 x2)/K1)
+alpha_function <- function(mu1,sig1,mu2,sig2,K1,K2,start,end){ # this is the function to compute the alpha coefficients from the mean and standard deviations of the Gaussian niches of the species and the start and end values of the environment
+  niche1 <- K1*dnorm(seq(start,end,length.out=100),mean=mu1,sd=sig1) # dnorm() generates the values of the Gaussian. Check ?dnorm
+  niche2 <- K2*dnorm(seq(start,end,length.out=100),mean=mu2,sd=sig2)
+  a <- sum(niche1*niche2)/sum(niche1*niche1) # because we have discrete values, we use a sum to approximate the integral
+  return(a)
+}
+
+# Let's try different parameter values
+D <- 10 # distance between the niche optima
+mu1 <- 5 # niche optima of species 1
+mu2 <- mu1+D # niche optima of species 2
+mu3 <- mu1+2*D # niche optima of species 3
+sig1 <- sig2 <- sig3 <- 10 # all species niches have the same standard deviation for simplicity
+start <- 0
+end <- 30
+K1 <- 200 # carrying capacity species 1 and 3
+K2 <- 250 # carrying capacity species 2
+a12 <- alpha_function(mu1,sig1,mu2,sig2,K1,K2,start,end)
+a13 <- alpha_function(mu1,sig1,mu3,sig3,K1,K1,start,end)
+a21 <- alpha_function(mu2,sig2,mu1,sig1,K2,K1,start,end)
+a23 <- alpha_function(mu2,sig2,mu3,sig3,K2,K1,start,end)
+a31 <- alpha_function(mu3,sig3,mu1,sig1,K1,K1,start,end)
+a32 <- alpha_function(mu3,sig3,mu2,sig2,K1,K2,start,end)
+
+# visualise the niches
+resource <- seq(start,end,length.out=100)
+niche1 <- dnorm(resource,mean=mu1,sd=sig1)*K1
+niche2 <- dnorm(resource,mean=mu2,sd=sig2)*K2
+niche3 <- dnorm(resource,mean=mu3,sd=sig3)*K1
+ggplot()+
+  geom_line(mapping=aes(x=resource,y=niche1),color="blue")+
+  geom_line(mapping=aes(x=resource,y=niche2),color="lightblue")+
+  geom_line(mapping=aes(x=resource,y=niche3),color="darkblue") +
+  theme_bw()
+
+# setup and solve the system of differential equations
+parameters <- c(a12=a12, a13=a13, a21=a21, a23=a23, a31=a31, a32=a32, r=0.3, K1 = K1, K2 = K2)
+state <- c(X1=10, X2=10, X3=10)
+
+lotka_volterra_three_species <- function(t,state,parameters){
+  with(as.list(c(state, parameters)),{
+    # rate of change
+    dX1 <- r*X1*(1-(X1+a12*X2+a13*X3)/K1)
+    dX2 <- r*X2*(1-(X2+a21*X1+a23*X3)/K2)
+    dX3 <- r*X3*(1-(X3+a31*X1+a32*X2)/K1)
+    
+    # return the rate of change
+    list(c(dX1, dX2, dX3))
+  }) # end with(as.list ...
+}
+
+times <- seq(0,200,by=0.01)
+out <- ode(y=state, times = times, func = lotka_volterra_three_species, parms = parameters)
+out_df <- data.frame(out)
+
+# plot the populations
+ggplot(data = out_df)+
+  geom_line(mapping=aes(x=time,y=X1),color="blue") +
+  geom_line(mapping=aes(x=time,y=X2),color="lightblue") +
+  geom_line(mapping=aes(x=time,y=X3),color="darkblue") +
+  geom_hline(yintercept=0,color="darkgrey") +
+  geom_vline(xintercept=0,color="darkgrey") +
+  labs(x = "Time", y = "P") +
+  theme_bw()
+# species 2 outcompetes both species 1 and 3
