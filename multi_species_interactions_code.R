ad11eafafa07bcb0ad811bfaa310a726bc8b6b3a
c-noonan-02 <charlottenoonan@outlook.com>
2024-10-15 15:03:54 +0100

    Completed step 3 - Incorporating a functional response.

--- multi_species_interactions_code.R
+++ multi_species_interactions_code.R
@@ -215,3 +215,134 @@ predator_prey_plot/predator_prey_logistic # far less oscillations, just oscillat
 phase_space_plot/phase_space_logistic # swirly fucker?? Ask about meaning? Chaotic effects?
 
 
+# STEP THREE: INCORPORTATING FUNCTIONAL RESPONSE
+
+# prey consumption by predators is not always linear
+# at some point there will be so many prey that predators will not be able to
+# hunt more than they already are - due to processing time etc.
+
+# the rate at which predators consume prey is called a functional response
+# three types - I, II, and III
+# type I (linear) is captured by the terms bxy and dxy in our existing model
+
+# dx / dt = ax - bxy / 1+A*x
+# dy / dt = cxy / 1+A*x - dy
+# A controls the slope of the functional response 
+# high A = poor hunting efficiency by the predator
+
+# type II functional response 
+x <- seq(0,50,0.1) 
+A <- 0.1
+y <- x / (1+A*x)
+
+# plot functional response with different values of A
+
+functional_response_0.1 <- ggplot() +
+  geom_line(mapping=aes(x=x,y=y),color="blue") +
+  geom_hline(yintercept=0,color="darkgrey") +
+  geom_vline(xintercept=0,color="darkgrey") +
+  labs(x = "Prey population", y = "Prey consumed") +
+  ggtitle("Functional Response II - A=0.1") +
+  theme_bw()
+
+A <- 0.2
+y <- x / (1+A*x)
+functional_response_0.2 <- ggplot() +
+  geom_line(mapping=aes(x=x,y=y),color="blue") +
+  geom_hline(yintercept=0,color="darkgrey") +
+  geom_vline(xintercept=0,color="darkgrey") +
+  labs(x = "Prey population", y = "Prey consumed") +
+  ggtitle("Functional Response II - A=0.2") +
+  theme_bw()
+
+A <- 0.4
+y <- x / (1+A*x)
+functional_response_0.4 <- ggplot() +
+  geom_line(mapping=aes(x=x,y=y),color="blue") +
+  geom_hline(yintercept=0,color="darkgrey") +
+  geom_vline(xintercept=0,color="darkgrey") +
+  labs(x = "Prey population", y = "Prey consumed") +
+  ggtitle("Functional Response II - A=0.4") +
+  theme_bw()
+
+A <- 0.6
+y <- x / (1+A*x)
+functional_response_0.6 <- ggplot() +
+  geom_line(mapping=aes(x=x,y=y),color="blue") +
+  geom_hline(yintercept=0,color="darkgrey") +
+  geom_vline(xintercept=0,color="darkgrey") +
+  labs(x = "Prey population", y = "Prey consumed") +
+  ggtitle("Functional Response II - A=0.6") +
+  theme_bw()
+
+A <- 0.8
+y <- x / (1+A*x)
+functional_response_0.8 <- ggplot() +
+  geom_line(mapping=aes(x=x,y=y),color="blue") +
+  geom_hline(yintercept=0,color="darkgrey") +
+  geom_vline(xintercept=0,color="darkgrey") +
+  labs(x = "Prey population", y = "Prey consumed") +
+  ggtitle("Functional Response II - A=0.8") +
+  theme_bw()
+
+A <- 1
+y <- x / (1+A*x)
+functional_response_1.0 <- ggplot() +
+  geom_line(mapping=aes(x=x,y=y),color="blue") +
+  geom_hline(yintercept=0,color="darkgrey") +
+  geom_vline(xintercept=0,color="darkgrey") +
+  labs(x = "Prey population", y = "Prey consumed") +
+  ggtitle("Functional Response II - A=1.0") +
+  theme_bw()
+
+functional_response_0.1+functional_response_0.2/functional_response_0.4+functional_response_0.6/functional_response_0.8+functional_response_1.0
+# curve becomes sharper as A increases
+
+# implement into the lotka volterra model
+# dx / dt = ax - bxy / 1+A*x
+# dy / dt = cxy / 1+A*x - dy
+lotka_volterra_type2 <- function(times,state,parameters){ #logistic growth function, that takes a set of parameter values, initial conditions and a time sequence
+  with(as.list(c(state, parameters)), { #"with" is a function that allows us to use the variable names directly - it looks for r, K and P in state and parameters
+    
+    dX <- a*X - b*X*Y/(1+A*X) # this is our equation for the prey dynamics, with logistic growth instead
+    dY <- c*X*Y/(1+A*X) - d*Y # this is the equation for predator dynamics
+    
+    # return the rate of change
+    list(c(dX, dY))
+  }) # end with(as.list ...
+}
+
+# initial population values
+state <- c(X=10, Y=10)
+# equation parameters
+parameters <- c(a=0.1, b=0.02, c=0.02, d=0.4, K=30, A=0.02)
+#sequence of time steps
+times <- seq(0,500,by=0.01)
+
+# call the function
+out <- ode(y=state, times = times, func = lotka_volterra_type2, parms = parameters)
+out_df <- data.frame(out)
+
+# we can then plot the output
+predator_prey_type2 <- ggplot(data = out_df)+
+  geom_line(mapping=aes(x=time,y=X),color="lightblue") +
+  geom_line(mapping=aes(x=time,y=Y),color="blue") +
+  geom_hline(yintercept=0,color="darkgrey") +
+  geom_vline(xintercept=0,color="darkgrey") +
+  labs(x = "Time", y = "P") +
+  ggtitle("Predator-Prey Cycles - type 2 functional response") +
+  theme_bw()
+phase_space_type2 <- ggplot(data = out_df)+
+  geom_path(mapping=aes(x=X,y=Y),color="blue") +
+  xlim(0,150) +
+  ylim(0,100) +
+  geom_hline(yintercept=0,color="darkgrey") +
+  geom_vline(xintercept=0,color="darkgrey") +
+  labs(x = "Prey", y = "Predator") +
+  ggtitle("Phase Space Plot - type 2 functional response") +
+  theme_bw()
+# check plots
+predator_prey_type2/phase_space_type2
+
+
+
